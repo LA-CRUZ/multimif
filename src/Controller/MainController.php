5efcf3770129e8c4e1f4acc91a43222ce3cb6fa6
Mickael Alves <0mickael.alves0@gmail.com>
2019-10-30 00:00:13 +0100

    Css for authentification pages, Closes #19

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,9 +15,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-/**
- * @Route("/{_locale}")
- */
 class MainController extends AbstractController
 {
     /**
