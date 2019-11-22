a271f619b1acacda8d40ef3df5df7537c6a587fb
Mickael Alves <0mickael.alves0@gmail.com>
2019-11-22 17:05:09 +0100

    Creator in quiz + List quiz by creator Closes #36 Closes 37

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -46,6 +46,7 @@ class MainController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
+            $quiz->setCreator($this->getUser());
             $manager->persist($quiz);
             $manager->flush();
 
@@ -139,7 +140,7 @@ class MainController extends AbstractController
     public function show_quiz_list(){
         $repo = $this->getDoctrine()->getRepository(Quiz::class);
 
-        $quiz_list = $repo->findAll();
+        $quiz_list = $repo->findBycreator($this->getUser());
 
         return $this->render('quiz/quiz_list.html.twig', [
             'controller_name' => 'MainController',
